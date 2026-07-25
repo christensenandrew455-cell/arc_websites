346c410eece76b31427d9415f2bc19634ad3d3b5
christensenandrew455-cell <christensenandrew455@gmail.com>
2026-07-24 21:55:15 -0400

    Update ARK Client Center routes

--- app/siteConfig.js
+++ app/siteConfig.js
@@ -1,22 +1,16 @@
 const siteConfig = {
-  // HEADER CONFIGURATION
   showHeader: true,
   stickyHeader: true,
-
-  // FOOTER
   showFooter: true,
   footerCentered: true,
-
-  // LAYOUT
   contentAlignment: "left",
   pagePadding: "p-6 md:p-12",
   maxWidth: "max-w-7xl",
-
-  // CONTACT PAGE
-  contactPageRoute: "/contact",
-
-  // PRIVACY PAGE
+  contactPageRoute: "/support",
+  supportPageRoute: "/support",
+  downloadPageRoute: "/download",
   privacyPageRoute: "/privacy",
+  termsPageRoute: "/terms",
 };
 
 export default siteConfig;
